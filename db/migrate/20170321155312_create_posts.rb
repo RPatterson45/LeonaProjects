de77d681e4c03f94338ce5afd75477e6fe7859f4
Ryan <Ryan_Patterson@yahoo.com>
2017-03-21 11:10:51 -0500

    Add post and comment scaffolds

---
+++ db/migrate/20170321155312_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.datetime :posted
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
