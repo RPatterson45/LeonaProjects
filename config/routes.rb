336a28ce8ef1cfa5e157213b4dfb0c75174a7cd2
Ryan <Ryan_Patterson@yahoo.com>
2017-03-27 19:54:08 -0500

    Change root path and add jumbotron

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-  root 'static_pages#index'
+  get 'posts/new'
+
+  get 'posts/edit'
+
+  root 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
